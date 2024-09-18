f66871f189e392e85ffbef7570aa2507d88c8259
Pawat <38448373+pawataven@users.noreply.github.com>
2024-09-18 10:08:48 +0700

    bank.dart

--- dart - Inheritance/bank.dart
+++ dart - Inheritance/bank.dart
@@ -1,29 +1,49 @@
-class BankAccount{
-  double? _balance;
+class BankAccount {
   String? _name;
+  double? _balance;
+  
+  BankAccount(this._name,this._balance);
 
-  BankAccount(this._balance, this._name);
-
-  double get balance => _balance!;
+  double get balance => this._balance!;
   set balance(double balance) => _balance = balance;
 
-  void deposite(double amount){
-    this._balance = balance + amount;
+  void deposit(double amount){
+    this._balance = balance + amount; 
+    print('ชื่อบัญชี : $_name');
+    print('ฝาก $amount');
+    print('จำนวนเงินคงเหลือ $_balance');
   }
-
   void withdraw(double amount){
     this._balance = balance - amount;
+    print('ชื่อบัญชี : $_name');
+    print('ถอน $amount');
+    print('จำนวนเงินคงเหลือ $_balance');
+  }
+  void showBalance(){
+    print('ชื่อบัญชี $_name');
+    print('จำนวนเงินคงเหลือ $_balance');
   }
 }
 
-class SavingAccount extends BankAccount{
-  double? _interest;
+class SavingAccount extends BankAccount {
+  double _interestRate;
+  SavingAccount(_name,_balance,this._interestRate):super(_name,_balance);
+  void addlnterest([double? newinterestRate]){
+    double x = balance * this._interestRate /100;
+    balance += x;
+    print('ชื่อบัญชี : $_name');
+    print('ดอกเบี้ย : $_interestRate เปอร์เซนต์');
+    print('ได้รับดอกเบี้ย : $x บาท');
+    print('จำนวนเงินคงเหลือ $_balance');
+  }
 
-  SavingAccount(balance, interest);
-
-  void addinterest()
+ 
 }
-
 void main(List<String> args) {
-  
+  var s = SavingAccount('Pawat Wantrong', 2000.00, 100);
+  s.showBalance();
+  s.deposit(400);
+  s.withdraw(100);
+  s.addlnterest(100);
+  s.showBalance();
 }
